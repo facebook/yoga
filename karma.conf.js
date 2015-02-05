58198d8102441cd4975ba8b6095d282184d6e956
daviskoh <koh.davis.0@gmail.com>
2015-02-04 21:56:40 -0500

    adhere to coding style

--- karma.conf.js
+++ karma.conf.js
@@ -1,42 +1,42 @@
 'use strict';
 
 module.exports = function (config) {
-    config.set({
+  config.set({
 
-        // base path, that will be used to resolve files and exclude
-        basePath: 'src',
+    // base path, that will be used to resolve files and exclude
+    basePath: 'src',
 
-        // frameworks to use
-        frameworks: ['jasmine'],
+    // frameworks to use
+    frameworks: ['jasmine'],
 
-        // list of files / patterns to load in the browser
-        files: [
-            'Layout.js',
-            'Layout-test-utils.js',
-            '__tests__/Layout-test.js',
-            '__tests__/Layout-consts-test.js'
-        ],
+    // list of files / patterns to load in the browser
+    files: [
+      'Layout.js',
+      'Layout-test-utils.js',
+      '__tests__/Layout-test.js',
+      '__tests__/Layout-consts-test.js'
+    ],
 
-        plugins: [
-            'karma-chrome-launcher',
-            'karma-jasmine'
-        ],
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-jasmine'
+    ],
 
-        // web server port
-        port: 9876,
+    // web server port
+    port: 9876,
 
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
-        browsers: ['Chrome'],
+    browsers: ['Chrome'],
 
-        // If browser does not capture in given timeout [ms], kill it
-        captureTimeout: 60000,
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
 
-        singleRun: false
-    });
+    singleRun: false
+  });
 };
