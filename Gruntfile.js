8ba0d1c608c1afe64c3c6b72d9ec7a20b1191e63
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-08-12 06:17:53 +0100

    Added transpile and 'c' tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,9 +10,14 @@ module.exports = function(grunt) {
       distFolder: 'dist',
       srcFolder: 'src',
       testFolder: 'src/__tests__',
+      cTestFiles: 'src/__tests__/Layout-test.c src/Layout.c src/Layout-test-utils.c',
+      cTestOutput: 'c_test'
     },
 
-    clean: ['<%= paths.distFolder %>'],
+    clean: {
+      dist: ['<%= paths.distFolder %>'],
+      ctest: ['<%= paths.cTestOutput %>']
+    },
 
     eslint: {
       options: {
@@ -57,10 +62,34 @@ module.exports = function(grunt) {
           singleRun: true
         }
       }
+    },
+
+    execute: {
+      transpile: {
+        src: ['<%= paths.srcFolder %>/transpile.js']
+      }
+    },
+
+    shell: {
+      ccompile: {
+        command: 'gcc -std=c99 -Werror -Wno-padded <%= paths.cTestFiles %> -lm -o "./<%= paths.cTestOutput %>"'
+      },
+      ctestExecute: {
+        command: './<%= paths.cTestOutput %>'
+      }
     }
   });
 
-  grunt.registerTask('build', ['eslint', 'clean', 'karma' ,'includereplace', 'uglify']);
+  
+  grunt.registerTask('ctest', ['shell:ccompile', 'shell:ctestExecute', 'clean:ctest']);
+
+  grunt.registerTask('transpile', ['execute:transpile', 'ctest']);
+
+  grunt.registerTask('test-javascript', ['eslint', 'karma']);
+
+  grunt.registerTask('package-javascript', ['clean:dist', 'includereplace', 'uglify']);
+
+  grunt.registerTask('build', ['test-javascript', 'transpile', 'package-javascript']);
 
   grunt.registerTask('default', ['build']);
 };
