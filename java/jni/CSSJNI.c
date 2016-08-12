e307dc22d1b34f98d8fb11961015dea797928137
Emil Sjolander <emilsj@fb.com>
2016-08-12 04:17:36 -0700

    Expose alignContent to java
    Summary: Expose already existing alignContent property to java
    
    Reviewed By: IanChilds
    
    Differential Revision: D3709071
    
    fbshipit-source-id: 421d75924ecc12d6d0975e342f3862cf5592f65f

--- java/jni/CSSJNI.c
+++ java/jni/CSSJNI.c
@@ -140,6 +140,7 @@ CSS_NODE_JNI_STYLE_PROP(jint, CSSFlexDirection, FlexDirection);
 CSS_NODE_JNI_STYLE_PROP(jint, CSSJustify, JustifyContent);
 CSS_NODE_JNI_STYLE_PROP(jint, CSSAlign, AlignItems);
 CSS_NODE_JNI_STYLE_PROP(jint, CSSAlign, AlignSelf);
+CSS_NODE_JNI_STYLE_PROP(jint, CSSAlign, AlignContent);
 CSS_NODE_JNI_STYLE_PROP(jint, CSSPositionType, PositionType);
 CSS_NODE_JNI_STYLE_PROP(jint, CSSWrapType, FlexWrap);
 CSS_NODE_JNI_STYLE_PROP(jint, CSSOverflow, Overflow);
