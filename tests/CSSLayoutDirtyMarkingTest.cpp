36eaae88e0a651f800f361245c4e8daec065f5d1
Emil Sjolander <emilsj@fb.com>
2016-08-31 08:02:39 -0700

    Add tests for dirty propagation
    Summary: Previous test for dirty propagation (last test in CSSLayoutTest.cpp) only tested one specific case. This tests much more of the bahavior to ensure that a vital optimization in css-layout is applied correctly.
    
    Reviewed By: lucasr
    
    Differential Revision: D3790968
    
    fbshipit-source-id: af11a5924640cd1d497920cd97549603a9f147cc

---
+++ tests/CSSLayoutDirtyMarkingTest.cpp
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#include <CSSLayout/CSSLayout.h>
+#include <gtest/gtest.h>
+
+TEST(CSSLayoutTest, dirty_propagation) {
+  const CSSNodeRef root = CSSNodeNew();
+  CSSNodeStyleSetAlignItems(root, CSSAlignFlexStart);
+  CSSNodeStyleSetWidth(root, 100);
+  CSSNodeStyleSetHeight(root, 100);
+
+  const CSSNodeRef root_child0 = CSSNodeNew();
+  CSSNodeStyleSetWidth(root_child0, 50);
+  CSSNodeStyleSetHeight(root_child0, 20);
+  CSSNodeInsertChild(root, root_child0, 0);
+
+  const CSSNodeRef root_child1 = CSSNodeNew();
+  CSSNodeStyleSetWidth(root_child1, 50);
+  CSSNodeStyleSetHeight(root_child1, 20);
+  CSSNodeInsertChild(root, root_child1, 1);
+
+  CSSNodeCalculateLayout(root, CSSUndefined, CSSUndefined, CSSDirectionLTR);
+
+  CSSNodeStyleSetWidth(root_child0, 20);
+
+  EXPECT_TRUE(CSSNodeIsDirty(root_child0));
+  EXPECT_FALSE(CSSNodeIsDirty(root_child1));
+  EXPECT_TRUE(CSSNodeIsDirty(root));
+
+  CSSNodeCalculateLayout(root, CSSUndefined, CSSUndefined, CSSDirectionLTR);
+
+  EXPECT_FALSE(CSSNodeIsDirty(root_child0));
+  EXPECT_FALSE(CSSNodeIsDirty(root_child1));
+  EXPECT_FALSE(CSSNodeIsDirty(root));
+}
+
+TEST(CSSLayoutTest, dirty_propagation_only_if_prop_changed) {
+  const CSSNodeRef root = CSSNodeNew();
+  CSSNodeStyleSetAlignItems(root, CSSAlignFlexStart);
+  CSSNodeStyleSetWidth(root, 100);
+  CSSNodeStyleSetHeight(root, 100);
+
+  const CSSNodeRef root_child0 = CSSNodeNew();
+  CSSNodeStyleSetWidth(root_child0, 50);
+  CSSNodeStyleSetHeight(root_child0, 20);
+  CSSNodeInsertChild(root, root_child0, 0);
+
+  const CSSNodeRef root_child1 = CSSNodeNew();
+  CSSNodeStyleSetWidth(root_child1, 50);
+  CSSNodeStyleSetHeight(root_child1, 20);
+  CSSNodeInsertChild(root, root_child1, 1);
+
+  CSSNodeCalculateLayout(root, CSSUndefined, CSSUndefined, CSSDirectionLTR);
+
+  CSSNodeStyleSetWidth(root_child0, 50);
+
+  EXPECT_FALSE(CSSNodeIsDirty(root_child0));
+  EXPECT_FALSE(CSSNodeIsDirty(root_child1));
+  EXPECT_FALSE(CSSNodeIsDirty(root));
+}
