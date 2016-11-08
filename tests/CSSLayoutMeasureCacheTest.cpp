12ebaa56b650f892f8cef52a63f1758e01951fcc
Emil Sjolander <emilsj@fb.com>
2016-11-08 15:23:08 -0800

    Dont measure single flex grow+shrink child
    Summary: If there is a single child which is flex grow and flex shrink then instead of measuring and then shrinking we can just set the flex basis to zero as we know the final result will be that the child take up all remaining space.
    
    Reviewed By: gkassabli
    
    Differential Revision: D4147298
    
    fbshipit-source-id: 51152e57eff8e322a833a6d698c30f8c5e2dcc35

--- tests/CSSLayoutMeasureCacheTest.cpp
+++ tests/CSSLayoutMeasureCacheTest.cpp
@@ -17,7 +17,8 @@ static CSSSize _measureMax(CSSNodeRef node,
                         CSSMeasureMode heightMode) {
 
   int *measureCount = (int *)CSSNodeGetContext(node);
-  *measureCount = *measureCount + 1;
+  (*measureCount)++;
+
   return CSSSize {
       .width = widthMode == CSSMeasureModeUndefined ? 10 : width,
       .height = heightMode == CSSMeasureModeUndefined ? 10 : height,
