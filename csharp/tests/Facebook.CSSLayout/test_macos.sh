cfd04ffa8e16a151a5ae817260a205bc7e16f1ca
Kazuki Sakamoto <kaz@fb.com>
2016-11-11 08:10:21 -0800

    Update .travis.yml for C# testing

--- csharp/tests/Facebook.CSSLayout/test_macos.sh
+++ csharp/tests/Facebook.CSSLayout/test_macos.sh
@@ -9,10 +9,12 @@ if mcs --version >/dev/null 2>&1; then true; else
   exit 1
 fi
 
-if mono64 --version >/dev/null 2>&1; then true; else
+if mono --version >/dev/null 2>&1; then true; else
   echo "ERROR: Can't execute mono64. You need to install Mono from http://www.mono-project.com/download/ and re-login to apply PATH environment."
   exit 1
 fi
+
+cd "$( dirname "$0" )"
 
 NUNIT=NUnit-2.6.4/bin
 if [ -d $NUNIT \
@@ -22,11 +24,10 @@ if [ -d $NUNIT \
     -a -f $NUNIT/lib/nunit.core.interfaces.dll \
     -a -f $NUNIT/lib/nunit.util.dll ]; then true; else
   curl -L -O https://github.com/nunit/nunitv2/releases/download/2.6.4/NUnit-2.6.4.zip
-  unzip NUnit-2.6.4.zip
+  unzip -qq NUnit-2.6.4.zip
   rm NUnit-2.6.4.zip
 fi
 
-cd "$( dirname "$0" )"
 clang -g -Wall -Wextra -dynamiclib -o libCSSLayout.dylib -I../../.. ../../../CSSLayout/*.c ../../CSSLayout/CSSInterop.cpp
 mcs -debug -t:library -r:$NUNIT/nunit.framework.dll -out:CSSLayoutTest.dll *.cs ../../../csharp/Facebook.CSSLayout/*cs
-MONO_PATH=$NUNIT mono64 --debug $NUNIT/nunit-console.exe CSSLayoutTest.dll
+MONO_PATH=$NUNIT mono --arch=64 --debug $NUNIT/nunit-console.exe CSSLayoutTest.dll
