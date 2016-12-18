e170dde4190d73d1131b4c5e1f64b2cd6a4c3ea3
David Hart <david@hartbit.com>
2016-12-18 17:21:27 +0100

    setup framework target in Xcode to support Carthage

---
+++ YogaKit/YogaKit/YogaKit/YogaKit.h
@@ -0,0 +1,19 @@
+//
+//  YogaKit.h
+//  YogaKit
+//
+//  Created by David Hart on 18.12.16.
+//  Copyright © 2016 facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YogaKit.
+FOUNDATION_EXPORT double YogaKitVersionNumber;
+
+//! Project version string for YogaKit.
+FOUNDATION_EXPORT const unsigned char YogaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YogaKit/PublicHeader.h>
+
+
