51380b276c98926606f70c163b8fda7ab392882e
Maël Nison <nison.mael@gmail.com>
2016-12-30 12:18:16 +0100

    Add facebook copyright banners

--- javascript/webpack.config.js
+++ javascript/webpack.config.js
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
 module.exports = {
 
     entry: "./sources/entry-browser",
@@ -23,6 +32,12 @@ module.exports = {
 
         hints: false
 
-    }
+    },
+
+    plugins: [
+
+        new (require("webpack").BannerPlugin)(require("fs").readFileSync(__dirname + "/../LICENSE", "utf8"))
+
+    ]
 
 };
