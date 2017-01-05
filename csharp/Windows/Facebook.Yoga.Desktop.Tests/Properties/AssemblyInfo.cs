bf6602ebffcbe87ca2579a2d1e3ec1c4d1f823f5
Andrew Forster <anforste@bluejeansnet.com>
2017-01-05 07:09:19 -0800

    Update C# wrapper to support both desktop and UWP projects
    Summary:
    The C# wrapper project has been changed to a NETStandard (1.1) PCL allowing it
    to be consumed by any project targeting .NET4.5 or greater including .NETCore
    and UWP projects... The C# wrapper uses P/Invoke to call into the Native Yoga
    DLL...
    
    The "Yoga" C++ project has been updated to support ARM builds...
    
    Added the ability to generate nuget packages for the C# wrapper that supports
    copying the native DLLs to the target output directory.
    
    Closes: https://github.com/facebook/yoga/pull/283
    
    Reviewed By: splhack, emilsjolander
    
    Differential Revision: D4381455
    
    Pulled By: JoelMarcey
    
    fbshipit-source-id: e6dc4190caa824ac63d5d5ee2eb4c7914efbb00f

---
+++ csharp/Windows/Facebook.Yoga.Desktop.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * Copyright (c) 2014-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Facebook.Yoga.Desktop.Tests")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Facebook, Inc.")]
+[assembly: AssemblyProduct("Facebook.Yoga")]
+[assembly: AssemblyCopyright("Copyright (c) 2014-present, Facebook, Inc.")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ac23f444-5545-4196-8b9f-5c1f6b3e7fb3")]
+
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
--- csharp/Windows/Facebook.Yoga/Properties/AssemblyInfo.cs
+++ csharp/Windows/Facebook.Yoga/Properties/AssemblyInfo.cs
@@ -11,22 +11,19 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+[assembly: AssemblyTitle("Facebook.Yoga")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Facebook, Inc.")]
 [assembly: AssemblyProduct("Facebook.Yoga")]
-[assembly: AssemblyTrademark("Copyright (c) 2014-present, Facebook, Inc.")]
+[assembly: AssemblyDescription("A subset of CSS's flexbox layout algorithm and box model.")]
+[assembly: AssemblyCopyright("Copyright (c) 2014-present, Facebook, Inc.")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("75bb7605-e54b-4ede-8f5a-ff1f24464236")]
 
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
