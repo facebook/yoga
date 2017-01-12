ea80f8bbf8fdc8afcaf20d2245f5b5755888b2a5
Kazuki Sakamoto <kaz@fb.com>
2017-01-11 21:35:15 -0800

    skip buck build if can't find buck

--- csharp/Mac/buck-build.sh
+++ csharp/Mac/buck-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if buck --version >/dev/null 2>&1; then true; else
-  echo "ERROR: Need to install buck https://buckbuild.com/setup/getting_started.html"
-  exit 1
+  echo "SKIP: Need to install buck https://buckbuild.com/setup/getting_started.html"
+  exit 0
 fi
 buck build $1
