9b7ef9a877330508235d4e2d58be27d8540f07c0
Rui Marinho <me@ruimarinho.net>
2017-01-14 00:59:07 +0000

    [csharp,android] Cleanup, remove extra files

--- csharp/Android/Facebook.Yoga.Android.Tests/MainActivity.cs
+++ csharp/Android/Facebook.Yoga.Android.Tests/MainActivity.cs
@@ -1,23 +1,17 @@
 ﻿using System.Reflection;
-
 using Android.App;
 using Android.OS;
 using Xamarin.Android.NUnitLite;
 
 namespace Facebook.Yoga.Android.Tests
 {
-    [Activity(Label = "Facebook.Yoga.Android.Tests", MainLauncher = true)]
-    public class MainActivity : TestSuiteActivity
-    {
-        protected override void OnCreate(Bundle bundle)
-        {
-            // tests can be inside the main assembly
-            AddTest(Assembly.GetExecutingAssembly());
-            // or in any reference assemblies
-            // AddTest (typeof (Your.Library.TestClass).Assembly);
-
-            // Once you called base.OnCreate(), you cannot add more assemblies.
-            base.OnCreate(bundle);
-        }
-    }
+	[Activity(Label = "Facebook.Yoga.Android.Tests", MainLauncher = true)]
+	public class MainActivity : TestSuiteActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			AddTest(Assembly.GetExecutingAssembly());
+			base.OnCreate(bundle);
+		}
+	}
 }
