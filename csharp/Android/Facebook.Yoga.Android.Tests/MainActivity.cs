4bc81972517d6fa3fb6b205289b3aab048a505c0
Rui Marinho <me@ruimarinho.net>
2017-01-14 00:34:15 +0000

    [csharp,android] Add Xamarin.Android support

---
+++ csharp/Android/Facebook.Yoga.Android.Tests/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+using Android.App;
+using Android.OS;
+using Xamarin.Android.NUnitLite;
+
+namespace Facebook.Yoga.Android.Tests
+{
+    [Activity(Label = "Facebook.Yoga.Android.Tests", MainLauncher = true)]
+    public class MainActivity : TestSuiteActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            // tests can be inside the main assembly
+            AddTest(Assembly.GetExecutingAssembly());
+            // or in any reference assemblies
+            // AddTest (typeof (Your.Library.TestClass).Assembly);
+
+            // Once you called base.OnCreate(), you cannot add more assemblies.
+            base.OnCreate(bundle);
+        }
+    }
+}
