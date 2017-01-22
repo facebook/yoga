bb7683a9be196041463a7e7b6605011760126a33
Lukas Wöhrl <lukas@woehrl.net>
2017-01-22 21:04:04 +0100

    java test

--- java/tests/com/facebook/yoga/YGAbsolutePositionTest.java
+++ java/tests/com/facebook/yoga/YGAbsolutePositionTest.java
@@ -425,6 +425,46 @@ public class YGAbsolutePositionTest {
   }
 
   @Test
+  public void test_absolute_layout_align_items_center_on_child_only() {
+    final YogaNode root = new YogaNode();
+    root.setFlexGrow(1f);
+    root.setWidth(110f);
+    root.setHeight(100f);
+
+    final YogaNode root_child0 = new YogaNode();
+    root_child0.setAlignSelf(YogaAlign.CENTER);
+    root_child0.setPositionType(YogaPositionType.ABSOLUTE);
+    root_child0.setWidth(60f);
+    root_child0.setHeight(40f);
+    root.addChildAt(root_child0, 0);
+    root.setDirection(YogaDirection.LTR);
+    root.calculateLayout();
+
+    assertEquals(0f, root.getLayoutX(), 0.0f);
+    assertEquals(0f, root.getLayoutY(), 0.0f);
+    assertEquals(110f, root.getLayoutWidth(), 0.0f);
+    assertEquals(100f, root.getLayoutHeight(), 0.0f);
+
+    assertEquals(25f, root_child0.getLayoutX(), 0.0f);
+    assertEquals(0f, root_child0.getLayoutY(), 0.0f);
+    assertEquals(60f, root_child0.getLayoutWidth(), 0.0f);
+    assertEquals(40f, root_child0.getLayoutHeight(), 0.0f);
+
+    root.setDirection(YogaDirection.RTL);
+    root.calculateLayout();
+
+    assertEquals(0f, root.getLayoutX(), 0.0f);
+    assertEquals(0f, root.getLayoutY(), 0.0f);
+    assertEquals(110f, root.getLayoutWidth(), 0.0f);
+    assertEquals(100f, root.getLayoutHeight(), 0.0f);
+
+    assertEquals(25f, root_child0.getLayoutX(), 0.0f);
+    assertEquals(0f, root_child0.getLayoutY(), 0.0f);
+    assertEquals(60f, root_child0.getLayoutWidth(), 0.0f);
+    assertEquals(40f, root_child0.getLayoutHeight(), 0.0f);
+  }
+
+  @Test
   public void test_absolute_layout_align_items_and_justify_content_center_and_top_position() {
     final YogaNode root = new YogaNode();
     root.setJustifyContent(YogaJustify.CENTER);
