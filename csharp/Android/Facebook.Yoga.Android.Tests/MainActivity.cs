34736a4929bf401a71cec008d2d2e2ddab3f58e2
Rui Marinho <me@ruimarinho.net>
2017-01-27 17:47:37 -0800

    Add Xamarin Android support
    Summary:
    Adds a Xamarin Android support for Yoga and respective tests. Closes #276 with this PR all Xamarin platforms will be covered
    
    Need to figure a better strategy for the build names for buck with splhack .
    
    It's failing 3 tests related with YogaNode and the GC :) classic!
    
    ![screenshot_1484353863](https://cloud.githubusercontent.com/assets/1235097/21950981/61020212-d9f8-11e6-80e1-185bd1707f9a.png)
    
    ```
    Facebook.Yoga.Android.Tests.dll : 660.87 ms
     : 678.788 ms
    Tests run: 130, Passed: 127, Failed: 3, Skipped: 0, Inconclusive: 0
    ```
    Closes https://github.com/facebook/yoga/pull/340
    
    Reviewed By: emilsjolander
    
    Differential Revision: D4475370
    
    Pulled By: splhack
    
    fbshipit-source-id: f050f10415e68e9808f629b843682b0f87cca065

---
+++ csharp/Android/Facebook.Yoga.Android.Tests/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * Copyright 2014-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE-examples file in the root directory of this source tree.
+ */
+
+using System.Reflection;
+using Android.App;
+using Android.OS;
+using Xamarin.Android.NUnitLite;
+
+namespace Facebook.Yoga.Android.Tests
+{
+	[Activity(Label = "Facebook.Yoga.Android.Tests", MainLauncher = true)]
+	public class MainActivity : TestSuiteActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			AddTest(Assembly.GetExecutingAssembly());
+			base.OnCreate(bundle);
+		}
+	}
+}
