abf912b729a46bed480753969a00236a75c9ce44
Robert Spencer <rspencer@fb.com>
2017-03-07 03:52:51 -0800

    Gradle version bump and POM details
    Summary: This improves the quality of the published POM files, adding dependencies and marking the object as an 'aar'.  It also bumps the version to 1.2.0 for JCenter.
    
    Reviewed By: emilsjolander
    
    Differential Revision: D4620150
    
    fbshipit-source-id: 968f1cea21af4b2f19aeff3f32ad575b185fa1bb

--- scripts/build_natives_for_gradle.sh
+++ scripts/build_natives_for_gradle.sh
@@ -3,14 +3,19 @@
 buck build //android:android
 
 X86_DEST=build/buck-out/jniLibs/x86
-ARMV7_DEST=build/buck-out/jniLibs/armabi-v7a
+ARMV7_DEST=build/buck-out/jniLibs/armeabi-v7a
 
 mkdir -p $X86_DEST
 mkdir -p $ARMV7_DEST
 
-cp ../buck-out/gen/java/jni#android-armv7,shared/libyoga.so $ARMV7_DEST
-cp ../buck-out/gen/java/jni#android-x86,shared/libyoga.so $X86_DEST 
+cp ../buck-out/gen/java/jni#ARMV7,android-armv7,android-strip,libyoga.so,shared/libyoga.so $ARMV7_DEST
+cp ../buck-out/gen/java/jni#X86,android-strip,android-x86,libyoga.so,shared/libyoga.so $X86_DEST
 
-cp ../buck-out/gen/yoga#android-armv7,shared/libyogacore.so $ARMV7_DEST
-cp ../buck-out/gen/yoga#android-x86,shared/libyogacore.so $X86_DEST
+cp ../buck-out/gen/yoga#ARMV7,android-armv7,android-strip,libyogacore.so,shared/libyogacore.so $ARMV7_DEST
+cp ../buck-out/gen/yoga#X86,android-strip,android-x86,libyogacore.so,shared/libyogacore.so $X86_DEST
 
+cp ../buck-out/gen/lib/fb/fbjni#ARMV7,android-armv7,android-strip,liblib_fb_fbjni.so,shared/liblib_fb_fbjni.so $ARMV7_DEST
+cp ../buck-out/gen/lib/fb/fbjni#X86,android-strip,android-x86,liblib_fb_fbjni.so,shared/liblib_fb_fbjni.so $X86_DEST
+
+cp ../buck-out/gen/android/android#ARMV7,android-strip,libgnustl_shared.so/libgnustl_shared.so $ARMV7_DEST
+cp ../buck-out/gen/android/android#X86,android-strip,libgnustl_shared.so/libgnustl_shared.so $X86_DEST
