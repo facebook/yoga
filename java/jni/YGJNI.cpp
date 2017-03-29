9af43090a6e4ac20e856ce776f851a5a73721470
Lukas Wöhrl <lukas@woehrl.net>
2017-03-29 16:39:24 -0700

    Don't transfer layout outputs to java for nodes which don't have a new layout
    Summary:
    As suggested in facebook/yoga#484. This is the PR which only adds the part of using a local bool field for storing the hasLayoutFlag, to be able to pass the layout only if it has really changed.
    Closes https://github.com/facebook/yoga/pull/492
    
    Reviewed By: astreet
    
    Differential Revision: D4786961
    
    Pulled By: emilsjolander
    
    fbshipit-source-id: cf3d354b93f6dcc3ef817ef73a47bd29e37d1848
    
    fail fast if ygnode gets GCed
    
    added backing field for new layout
    
    fix compile
    
    set padding, margin and border only if set
    
    changed from bool to int to reduce jni calls by 2
    
    code review

--- java/jni/YGJNI.cpp
+++ java/jni/YGJNI.cpp
@@ -47,35 +47,51 @@ static void YGTransferLayoutOutputsRecursive(YGNodeRef root) {
       static auto borderBottomField = obj->getClass()->getField<jfloat>("mBorderBottom");
 
       static auto hasNewLayoutField = obj->getClass()->getField<jboolean>("mHasNewLayout");
+    static auto edgeSetFlagField = obj->getClass()->getField<jint>("mEdgeSetFlag");
+
+    /* Those flags needs be in sync with YogaNode.java */
+    const int MARGIN = 1;
+    const int PADDING = 2;
+    const int BORDER = 4;
+
+    int hasEdgeSetFlag = (int)obj->getFieldValue(edgeSetFlagField);
 
       obj->setFieldValue(widthField, YGNodeLayoutGetWidth(root));
       obj->setFieldValue(heightField, YGNodeLayoutGetHeight(root));
       obj->setFieldValue(leftField, YGNodeLayoutGetLeft(root));
       obj->setFieldValue(topField, YGNodeLayoutGetTop(root));
 
+    if((hasEdgeSetFlag & MARGIN) == MARGIN){
       obj->setFieldValue(marginLeftField, YGNodeLayoutGetMargin(root, YGEdgeLeft));
       obj->setFieldValue(marginTopField, YGNodeLayoutGetMargin(root, YGEdgeTop));
       obj->setFieldValue(marginRightField, YGNodeLayoutGetMargin(root, YGEdgeRight));
       obj->setFieldValue(marginBottomField, YGNodeLayoutGetMargin(root, YGEdgeBottom));
+    }
 
+    if((hasEdgeSetFlag & PADDING) == PADDING){
       obj->setFieldValue(paddingLeftField, YGNodeLayoutGetPadding(root, YGEdgeLeft));
       obj->setFieldValue(paddingTopField, YGNodeLayoutGetPadding(root, YGEdgeTop));
       obj->setFieldValue(paddingRightField, YGNodeLayoutGetPadding(root, YGEdgeRight));
       obj->setFieldValue(paddingBottomField, YGNodeLayoutGetPadding(root, YGEdgeBottom));
+    }
 
+    if((hasEdgeSetFlag & BORDER) == BORDER){
       obj->setFieldValue(borderLeftField, YGNodeLayoutGetBorder(root, YGEdgeLeft));
       obj->setFieldValue(borderTopField, YGNodeLayoutGetBorder(root, YGEdgeTop));
       obj->setFieldValue(borderRightField, YGNodeLayoutGetBorder(root, YGEdgeRight));
       obj->setFieldValue(borderBottomField, YGNodeLayoutGetBorder(root, YGEdgeBottom));
+    }
 
       obj->setFieldValue<jboolean>(hasNewLayoutField, true);
       YGTransferLayoutDirection(root, obj);
       YGNodeSetHasNewLayout(root, false);
+  } else {
+    YGLog(YGLogLevelError, "Java YGNode was GCed during layout calculation\n");
+    return;
+  }
       for (uint32_t i = 0; i < YGNodeGetChildCount(root); i++) {
         YGTransferLayoutOutputsRecursive(YGNodeGetChild(root, i));
       }
-    } else {
-      YGLog(YGLogLevelError, "Java YGNode was GCed during layout calculation\n");
     }
   }
 }
