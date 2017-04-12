c080a46571fb87f2fc827c7ac093d41671c148e3
Emil Sjolander <emilsj@fb.com>
2017-04-12 09:52:30 -0700

    Fix some issues with the deploy script and deploy a new version for android
    Summary: Fix some issues with the deploy script and deploy a new version for android
    
    Reviewed By: passy
    
    Differential Revision: D4876006
    
    fbshipit-source-id: ddafb8349e1c77ab5afd9a823103fb6a1dfabb1d

--- scripts/build_natives_for_gradle.sh
+++ scripts/build_natives_for_gradle.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+BASEDIR=$(dirname $0)
+
 DESTINATIONS=(
   "build/buck-out/jniLibs/x86"
   "build/buck-out/jniLibs/x86_64"
@@ -22,22 +25,22 @@ done
 # There must be a better way to get gnustl_shared than building the android target.
 # But for now, we include that target simply for that shared library...
 echo "Build libgnustl_shared.so"
-~/mybuck/bin/buck build //android/sample:sample
+buck build //android/sample:sample
 
-cp "../buck-out/gen/android/sample/sample#X86,android-strip,libgnustl_shared.so/libgnustl_shared.so" "${DESTINATIONS[0]}"
-cp "../buck-out/gen/android/sample/sample#X86_64,android-strip,libgnustl_shared.so/libgnustl_shared.so" "${DESTINATIONS[1]}"
-cp "../buck-out/gen/android/sample/sample#ARMV7,android-strip,libgnustl_shared.so/libgnustl_shared.so" "${DESTINATIONS[2]}"
-cp "../buck-out/gen/android/sample/sample#ARM64,android-strip,libgnustl_shared.so/libgnustl_shared.so" "${DESTINATIONS[3]}"
+cp "$BASEDIR/../buck-out/gen/android/sample/sample#X86,android-strip,libgnustl_shared.so/libgnustl_shared.so" "${DESTINATIONS[0]}"
+cp "$BASEDIR/../buck-out/gen/android/sample/sample#X86_64,android-strip,libgnustl_shared.so/libgnustl_shared.so" "${DESTINATIONS[1]}"
+cp "$BASEDIR/../buck-out/gen/android/sample/sample#ARMV7,android-strip,libgnustl_shared.so/libgnustl_shared.so" "${DESTINATIONS[2]}"
+cp "$BASEDIR/../buck-out/gen/android/sample/sample#ARM64,android-strip,libgnustl_shared.so/libgnustl_shared.so" "${DESTINATIONS[3]}"
 
 # This is to clean up after the mess above.  Yes, it is required.
-~/mybuck/bin/buck clean
-rm -r ../buck-out
+buck clean
+rm -r "$BASEDIR/../buck-out"
 
 for (( i=0; i<4; i++ ));
 do
   echo "Build ${BUCK_TARGETS[i]}"
   buck build "//java:jni#${BUCK_TARGETS[i]},shared"
-  cp "../buck-out/gen/java/jni#${BUCK_TARGETS[i]},shared/libyoga.so" "${DESTINATIONS[i]}"
-  cp "../buck-out/gen/yoga#${BUCK_TARGETS[i]},shared/libyogacore.so" "${DESTINATIONS[i]}"
-  cp "../buck-out/gen/lib/fb/fbjni#${BUCK_TARGETS[i]},shared/liblib_fb_fbjni.so" "${DESTINATIONS[i]}"
+  cp "$BASEDIR/../buck-out/gen/java/jni#${BUCK_TARGETS[i]},shared/libyoga.so" "${DESTINATIONS[i]}"
+  cp "$BASEDIR/../buck-out/gen/yoga#${BUCK_TARGETS[i]},shared/libyogacore.so" "${DESTINATIONS[i]}"
+  cp "$BASEDIR/../buck-out/gen/lib/fb/fbjni#${BUCK_TARGETS[i]},shared/liblib_fb_fbjni.so" "${DESTINATIONS[i]}"
 done
