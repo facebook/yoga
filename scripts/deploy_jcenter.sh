6be1c2cdb4309087437aacf4ef050f2b0de428d4
Pascal Hartig <realpassy@fb.com>
2017-04-19 12:22:09 -0700

    Begone buckBuildAndCopy!
    Summary:
    I'm actually quite excited about this one!
    
    No more buck shelling out for building the Yoga AARs/JARs via Gradle. It's now
    all done via Gradle.
    
    This commit is the only one that should actually change anything about the entry
    points to the gradle builds and release process. **So if anything goes wrong
    with the next release, reverting this one here should be enough!**
    
    Reviewed By: emilsjolander
    
    Differential Revision: D4913600
    
    fbshipit-source-id: 4a54562ad5be69f62a7781d43fddad211f99ab25

--- scripts/deploy_jcenter.sh
+++ scripts/deploy_jcenter.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
+ROOTDIR="$(dirname $0)/.."
 echo -e "\033[1;34m** We can deploy two libraries\n** (1) Java bindings to Yoga\n** (2) Android YogaLayout\n** Which do you want to ship today? \033[0m"
 read -p "" -n 1 mod
 case $mod in
-  1 ) MODULE="java";;
-  2 ) MODULE="android";;
+  1 ) MODULE="yoga";;
+  2 ) MODULE="yoga-layout";;
   * ) echo -e "\n\033[1;34m** Invalid selection" && exit
   esac
 
@@ -17,12 +18,12 @@ read -r BINTRAY_KEY
 echo -e "\033[1;34m** [3/3] Please enter your GPG passphrase: \033[0m"
 read -r GPG_PASS
 
-uploadcmd="gradle clean build bintrayUpload --info -PbintrayUsername='$BINTRAY_USER' -PbintrayApiKey='$BINTRAY_KEY' -PbintrayGpgPassword='$GPG_PASS'"
+uploadcmd="$ROOTDIR/gradlew clean :${MODULE}:build bintrayUpload --info -PbintrayUsername='$BINTRAY_USER' -PbintrayApiKey='$BINTRAY_KEY' -PbintrayGpgPassword='$GPG_PASS'"
 
 echo
 echo -e "\033[1;34m** Dry run\033[0m"
 
-(cd $MODULE ; eval "$uploadcmd -PdryRun=true")
+eval "$uploadcmd -PdryRun=true"
 
 echo
 echo -e "\033[1;34m** Are you happy to conintue?: [yN]\033[0m"
@@ -35,4 +36,4 @@ read -p "" -n 1 yn
 echo
 echo -e "\033[1;34m** Publishing\033[0m"
 
-(cd $MODULE ; eval "$uploadcmd")
+eval "$uploadcmd"
