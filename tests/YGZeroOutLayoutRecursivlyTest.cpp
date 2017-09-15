bcc36cc1b2a8e90a1bc2142b8615c84afc81b31c
Mingwei Pan <mingwei@fb.com>
2017-09-15 08:22:36 -0700

    Zero out all fields in YGZeroOutLayoutRecursivly using memset
    Summary:
    Add a new test named YGZeroOutLayoutRecursivlyTest, which verifies that padding and margin of a child node should be set to 0 when the display is set to none.
    Modify YGZeroOutLayoutRecursivly function, use memset to clear the layout struct of the node.
    
    Reviewed By: emilsjolander
    
    Differential Revision: D5842351
    
    fbshipit-source-id: d2c2b6f829588a200525b7ccbdb63003cb4dcc4e

---
+++ tests/YGZeroOutLayoutRecursivlyTest.cpp
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#include <gtest/gtest.h>
+#include <yoga/Yoga.h>
+
+TEST(YogaTest, zero_out_layout) {
+  const YGNodeRef root = YGNodeNew();
+  YGNodeStyleSetFlexDirection(root, YGFlexDirectionRow);
+  YGNodeStyleSetWidth(root, 200);
+  YGNodeStyleSetHeight(root, 200);
+
+  const YGNodeRef child = YGNodeNew();
+  YGNodeInsertChild(root, child, 0);
+  YGNodeStyleSetWidth(child, 100);
+  YGNodeStyleSetHeight(child, 100);
+  YGNodeStyleSetMargin(child, YGEdgeTop, 10);
+  YGNodeStyleSetPadding(child, YGEdgeTop, 10);
+
+  YGNodeCalculateLayout(root, 100, 100, YGDirectionLTR);
+
+  ASSERT_FLOAT_EQ(10, YGNodeLayoutGetMargin(child, YGEdgeTop));
+  ASSERT_FLOAT_EQ(10, YGNodeLayoutGetPadding(child, YGEdgeTop));
+
+  YGNodeStyleSetDisplay(child, YGDisplayNone);
+
+  YGNodeCalculateLayout(root, 100, 100, YGDirectionLTR);
+
+  ASSERT_FLOAT_EQ(0, YGNodeLayoutGetMargin(child, YGEdgeTop));
+  ASSERT_FLOAT_EQ(0, YGNodeLayoutGetPadding(child, YGEdgeTop));
+
+  YGNodeFreeRecursive(root);
+}
