a2b6ddb7b1371e239627d319a3a72efe229797b1
Sophie Alpert <sophiebits@fb.com>
2018-02-16 18:24:55 -0800

    Update license headers for MIT license
    Summary:
    Includes React Native and its dependencies Fresco, Metro, and Yoga. Excludes samples/examples/docs.
    
    find: ^(?:( *)|( *(?:[\*~#]|::))( )? *)?Copyright (?:\(c\) )?(\d{4})\b.+Facebook[\s\S]+?BSD[\s\S]+?(?:this source tree|the same directory)\.$
    replace: $1$2$3Copyright (c) $4-present, Facebook, Inc.\n$2\n$1$2$3This source code is licensed under the MIT license found in the\n$1$2$3LICENSE file in the root directory of this source tree.
    
    Reviewed By: TheSavior, yungsters
    
    Differential Revision: D7007050
    
    fbshipit-source-id: 37dd6bf0ffec0923bfc99c260bb330683f35553e

--- csharp/Android/Facebook.Yoga.Android.Tests/MainActivity.cs
+++ csharp/Android/Facebook.Yoga.Android.Tests/MainActivity.cs
@@ -1,9 +1,8 @@
 ﻿/**
- * Copyright 2014-present, Facebook, Inc.
- * All rights reserved.
+ * Copyright (c) 2014-present, Facebook, Inc.
  *
- * This source code is licensed under the license found in the
- * LICENSE-examples file in the root directory of this source tree.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 using System.Reflection;
