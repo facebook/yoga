6d7eca8e421961228198b16c0ca7601156a561ff
Pascal Hartig <realpassy@fb.com>
2018-02-21 09:07:29 -0800

    Fix typo in docs
    Summary:
    "feature" should be "features" but it's already used twice in that paragraph so
    I replaced it with "showcases" instead.
    
    Reviewed By: emilsjolander
    
    Differential Revision: D7042770
    
    fbshipit-source-id: 0287f8746feca5c9f651950c8e9c24d2e86d55e9

--- website/src/pages/docs/index.js
+++ website/src/pages/docs/index.js
@@ -32,7 +32,7 @@ export default ({data}) => (
             Welcome to Yoga's documentation page. Below you will find helpful
             documentation covering all the features of the library. Each page
             comes with an interactive playground for you to explore that
-            feature. The examples section feature some of the most common
+            feature. The examples section showcases some of the most common
             layouts and how to build them. This is a community projects and
             contributions within documentation, code, and tests are more then
             welcome. The contributing section below covers how to get started.
