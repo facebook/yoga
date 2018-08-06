78cdf3cadcae48825d644a1c04128faec1511e83
David Aurelio <davidaurelio@fb.com>
2018-08-06 02:10:43 -0700

    Back to JNI storage
    Summary:
    @public
    
    This reverts the Yoga/Java storage experiment. I will follow up with any learnings.
    
    Reviewed By: pasqualeanatriello
    
    Differential Revision: D9168405
    
    fbshipit-source-id: fb227fb9353bd4c4e3bebbe9b04eec1132e532e8

--- java/tests/com/facebook/yoga/TestParametrization.java
+++ java/tests/com/facebook/yoga/TestParametrization.java
@@ -8,74 +8,27 @@
 package com.facebook.yoga;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class TestParametrization {
   public static Iterable<NodeFactory> nodeFactories() {
-    return Arrays.asList(
-      new NodeFactory() {
-        @Override
-        public YogaNode create() {
-          return new YogaNode();
-        }
+    NodeFactory nodeFactory = new NodeFactory() {
+      @Override
+      public YogaNode create() {
+        return new YogaNode();
+      }
 
-        @Override
-        public YogaNode create(YogaConfig config) {
-          return new YogaNode(config);
-        }
+      @Override
+      public YogaNode create(YogaConfig config) {
+        return new YogaNode(config);
+      }
 
-        @Override
-        public String toString() {
-          return "JNI";
-        }
-      },
-      new NodeFactory() {
-        @Override
-        public YogaNode create() {
-          return new YogaNode(YogaNode.BYTE_BUFFER);
-        }
-
-        @Override
-        public YogaNode create(YogaConfig config) {
-          return new YogaNode(YogaNode.BYTE_BUFFER, config);
-        }
-
-        @Override
-        public String toString() {
-          return "ByteBuffer";
-        }
-      },
-      new NodeFactory() {
-        @Override
-        public YogaNode create() {
-          return new YogaNode(YogaNode.HYBRID);
-        }
-
-        @Override
-        public YogaNode create(YogaConfig config) {
-          return new YogaNode(YogaNode.HYBRID, config);
-        }
-
-        @Override
-        public String toString() {
-          return "Hybrid";
-        }
-      },
-      new NodeFactory() {
-        @Override
-        public YogaNode create() {
-          return new YogaNode(YogaNode.UNSAFE);
-        }
-
-        @Override
-        public YogaNode create(YogaConfig config) {
-          return new YogaNode(YogaNode.UNSAFE, config);
-        }
-
-        @Override
-        public String toString() {
-          return "Unsafe";
-        }
-      });
+      @Override
+      public String toString() {
+        return "JNI";
+      }
+    };
+    return Arrays.asList(nodeFactory);
   }
 
 
