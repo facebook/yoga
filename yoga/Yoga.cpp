3b2dee86838b27221fa384ed63e7357e82279d21
Daniel Hood <hood.danielc@gmail.com>
2018-10-25 23:29:51 -0700

    Use YGLayout default constructor instead of memset for YGZeroOutLayoutRecursivly

--- yoga/Yoga.cpp
+++ yoga/Yoga.cpp
@@ -1851,7 +1851,9 @@ static bool YGNodeFixedSizeSetMeasuredDimensions(
 }
 
 static void YGZeroOutLayoutRecursivly(const YGNodeRef node) {
-  memset(static_cast<void*>(&(node->getLayout())), 0, sizeof(YGLayout));
+  node->getLayout() = {};
+  node->setLayoutDimension(0, 0);
+  node->setLayoutDimension(0, 1);
   node->setHasNewLayout(true);
   node->cloneChildrenIfNeeded();
   const uint32_t childCount = YGNodeGetChildCount(node);
