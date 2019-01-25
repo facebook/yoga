89606b94e47817143a4c7c19a6ac6b0aee410161
Sam Mueller <sjmueller@gmail.com>
2019-01-24 23:41:28 -0800

    Add carthage support

--- YogaKit/Source/YGLayout.h
+++ YogaKit/Source/YGLayout.h
@@ -5,9 +5,9 @@
  * file in the root directory of this source tree.
  */
 #import <UIKit/UIKit.h>
-#import <yoga/YGEnums.h>
-#import <yoga/Yoga.h>
-#import <yoga/YGMacros.h>
+#import <YogaKit/YGEnums.h>
+#import <YogaKit/Yoga.h>
+#import <YogaKit/YGMacros.h>
 
 YG_EXTERN_C_BEGIN
 
