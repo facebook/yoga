a4bdd9cd9b459cad7540d7ad7e099105a14e1a73
Rain ⁣ <rain1@fb.com>
2019-06-06 19:36:56 -0700

    standardize C-like MIT copyright headers throughout fbsource
    Summary:
    `/*` is the standard throughout open source code. For example, Firefox uses single /*: https://hg.mozilla.org/mozilla-central/file/21d22b2f541258d3d1cf96c7ba5ad73e96e616b5/gfx/ipc/CompositorWidgetVsyncObserver.cpp#l3
    
    In addition, Rust considers `/**` to be a doc comment (similar to Javadoc) and having such a comment at the beginning of the file causes `rustc` to barf.
    
    Note that some JavaScript tooling requires `/**`. This is OK since JavaScript files were not covered by the linter in the first place, but it would be good to have that tooling fixed too.
    
    Reviewed By: zertosh
    
    Differential Revision: D15640366
    
    fbshipit-source-id: b4ed4599071516364d6109720750d6a43304c089

--- csharp/Yoga/resource.h
+++ csharp/Yoga/resource.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
+ */
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by Yoga.rc
