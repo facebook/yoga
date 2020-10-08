ee0e3ade74a2487f5151e6b158cae2b84a39b08e
Jonas Salling <jonas.salling@schibsted.com>
2020-10-09 00:00:39 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,26 +6,27 @@ import PackageDescription
 let package = Package(
     name: "yoga",
     platforms: [
-		.iOS(.v14),
-	],
+        .iOS(.v14),
+    ],
     products: [
         .library(
             name: "yoga",
+            type: .dynamic,
             targets: ["yoga"]
-		),
+        ),
     ],
     targets: [
         .target(
             name: "yoga",
             dependencies: [],
-			path: "./yoga",
-			cxxSettings: [
-				.headerSearchPath("../"),
-			],
-      linkerSettings: [
-        .linkedFramework("Foundation"),
-      ]
-		)
+            path: "./yoga",
+            cxxSettings: [
+                .headerSearchPath("../"),
+            ],
+            linkerSettings: [
+                .linkedFramework("Foundation"),
+            ]
+        )
     ],
     cxxLanguageStandard: .cxx14
 )
