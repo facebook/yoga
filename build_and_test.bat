a17788fab11ba91f2acad4b5f4860ffac965ea93
Nick Gerleman <ngerlem@fb.com>
2022-12-31 07:46:50 -0800

    .

--- build_and_test.bat
+++ build_and_test.bat
@@ -6,19 +6,19 @@
 @echo off
 SETLOCAL
 
-cd tests
+cd "%0\..\tests"
 
 if "%1" == "" (
-  build_type="Debug"
+  set build_type="Debug"
 ) else (
-  build_type="%1"
+  set build_type="%1"
 )
 
 where ninja
 if errorlevel 1 (
-  cmake -B build -S . -D CMAKE_BUILD_TYPE=$build_type -G Ninja
-) else  (
   cmake -B build -S . -D CMAKE_BUILD_TYPE=$build_type
+) else  (
+  cmake -B build -S . -D CMAKE_BUILD_TYPE=$build_type -G Ninja
 )
 if %errorlevel% neq 0 exit /b %errorlevel%
 
