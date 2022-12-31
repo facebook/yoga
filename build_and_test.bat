50a0bced9a2f3cae5506a36129ee91dbf2ea3425
Nick Gerleman <ngerlem@fb.com>
2022-12-31 07:47:12 -0800

    echo

--- build_and_test.bat
+++ build_and_test.bat
@@ -3,7 +3,7 @@
 @REM This source code is licensed under the MIT license found in the
 @REM LICENSE file in the root directory of this source tree.
 
-@echo off
+@REM @echo off
 SETLOCAL
 
 cd "%0\..\tests"
