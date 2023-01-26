4df43d48f5d4af99774689b4d6d2bfcf970abc08
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-01-26 17:48:25 +0300

    fix test

--- javascript/tests/YGMeasureTest.test.js
+++ javascript/tests/YGMeasureTest.test.js
@@ -41,9 +41,9 @@ test("dont_fail_with_incomplete_measure_dimensions", () => {
   root.insertChild(node2, root.getChildCount());
   root.insertChild(node3, root.getChildCount());
 
-  node1.setMeasureFunc(heightOnlyCallback);
-  node1.setMeasureFunc(widthOnlyCallback);
-  node1.setMeasureFunc(emptyCallback);
+  node1.setMeasureFunc(heightOnlyCallback.inc);
+  node1.setMeasureFunc(widthOnlyCallback.inc);
+  node1.setMeasureFunc(emptyCallback.inc);
 
   root.calculateLayout(Yoga.UNDEFINED, Yoga.UNDEFINED, Yoga.DIRECTION_LTR);
 
