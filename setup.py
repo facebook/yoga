072a23f05971a1d3d6220f728b27aee39367fac5
Nicolas Martinez <17040442+nicomt@users.noreply.github.com>
2023-03-07 00:08:03 -0500

    Fixes for build config

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from glob import glob
 from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension, build_ext
+from pybind11.setup_helpers import Pybind11Extension
 
 setup(
     name="yoga-layout",
@@ -10,11 +10,14 @@ setup(
     ext_modules=[
         Pybind11Extension(
             "yoga",
-            [*sorted(glob("yoga/**/*.cpp")), *sorted(glob("python/src/*.cc"))],
+            [
+                *sorted(glob("yoga/*.cpp")),
+                *sorted(glob("yoga/**/*.cpp")),
+                *sorted(glob("python/src/*.cc")),
+            ],
             include_dirs=["."],
         )
     ],
-    cmdclass={"build_ext": build_ext},
     zip_safe=False,
     extras_require={"test": ["pytest>=6.0"], "build": ["pybind11>=2.10.3"]},
     python_requires=">=3.7",
