420f0016c646bf283c6c5346630a0f10f7234a43
Nicola Corti <ncor@meta.com>
2023-05-09 09:48:55 -0700

    Hook Yoga's Gradle build into Gradle Enterprise
    Summary: This adds the setup necessary to hook Yoga into a Gradle Enterprise instance
    
    Reviewed By: mdvacca
    
    Differential Revision: D45694413
    
    fbshipit-source-id: 766ea7e05f99ae347853dbe3817521379b4e44ba

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+plugins { id("com.gradle.enterprise").version("3.7.1") }
+
+include(":yoga", ":yoga-layout")
+project(":yoga").projectDir = file("java")
+project(":yoga-layout").projectDir = file("android")
+
+rootProject.name = "yoga-github"
+
+// If you specify a file inside gradle/gradle-enterprise.gradle.kts
+// you can configure your custom Gradle Enterprise instance
+if (file("./gradle/gradle-enterprise.gradle.kts").exists()) {
+  apply(from = "./gradle/gradle-enterprise.gradle.kts")
+}
