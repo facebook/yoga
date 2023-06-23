6e441976454fd5616d1c8513029712405160e1c7
Nick Gerleman <ngerlem@meta.com>
2023-06-23 13:43:52 -0700

    Docusarus: Init Project
    Summary:
    This starts us transitioning the Yoga website off of an ancient version of Gatsby by initializing a new Docusaurus project. This commit adds a new stock project, to be filled in later, then fixes license lint errors.
    
    Note that this seems to have init'd using npm, but in the next diff I convert this to yarn workspaces anyway.
    
    Differential Revision: D46884434
    
    fbshipit-source-id: 26e2ecba057ae99c4ef878e4cd21c8d21a25c2b5

---
+++ website-next/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/ngerlem/fbsource/xplat/yoga/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/ngerlem/fbsource/xplat/yoga/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/ngerlem/fbsource/xplat/yoga/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/ngerlem/fbsource/xplat/yoga/website-next/src/css/custom.css'),
+];
