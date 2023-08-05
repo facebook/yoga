271ecdb92b091cfc4dad38f627f1a5fad5be0810
Lvv.me <cntrump@gmail.com>
2023-08-05 13:16:56 +0800

    Add SwiftPM support

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import PackageDescription
+
+let package = Package(
+    name: "yoga",
+    products: [
+        .library(name: "yoga", targets: [ "C" ])
+    ],
+    targets: [
+        .target(
+            name: "C",
+            path: ".",
+            sources: [
+                "yoga"
+            ],
+            publicHeadersPath: ".",
+            cxxSettings: [
+                .headerSearchPath(".")
+            ]
+        )
+    ],
+    cxxLanguageStandard: CXXLanguageStandard(rawValue: "c++17")
+)
