bb892af3a4b8cd7ad0d091b8eadfddfa8e83c763
Nick Gerleman <ngerlem@meta.com>
2023-11-13 22:09:35 -0800

    Make trunk builds "0.0.0" (#1466)
    Summary:
    X-link: https://github.com/facebook/react-native/pull/41423
    
    Pull Request resolved: https://github.com/facebook/yoga/pull/1466
    
    Right now Yoga's main branch says it's 2.0.0, and RN's dirsync says its 1.14.0, but the code is really closer to what will be Yoga 3.0.0.
    
    This changes trunk builds to "0.0.0" for clarity, which will be assigned a real version number the first time publishing a new Yoga branch.
    
    This is separately a good practice to prevent the chance of accidental publishes causing damage.
    
    Changelog: [Internal]
    
    Reviewed By: christophpurrer
    
    Differential Revision: D51236778
    
    fbshipit-source-id: 06cac89bcca1c707ce5c00f9c346f627eef6b4bc

--- set-version.py
+++ set-version.py
@@ -28,6 +28,14 @@ with open("javascript/package.json", "r+") as f:
     f.truncate()
     f.write(new_contents)
 
+with open("website-next/package.json", "r+") as f:
+    new_contents = re.sub(
+        r'"yoga-layout": ".*"', f'"yoga-layout": "{version}"', f.read()
+    )
+    f.seek(0)
+    f.truncate()
+    f.write(new_contents)
+
 with open("Yoga.podspec", "r+") as f:
     new_contents = re.sub(
         r"spec\.version = '.*'", f"spec.version = '{version}'", f.read()
