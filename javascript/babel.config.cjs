e84056ad30efbbe10ffce5655fd59d1cef164d5d
Nick Gerleman <ngerlem@meta.com>
2023-12-12 04:11:25 -0800

    Use repo root babel config in /javascript (#1509)
    Summary:
    Pull Request resolved: https://github.com/facebook/yoga/pull/1509
    
    D51966201 adds a new Babel config to the repo root. This makes it so the existing JS package shares that, and shifts around the dependencies a bit, to avoid reliance on our specific hoisting resolution.
    
    Babel's config structure makes it annoying to be able to resolve a babel config across a package boundary unless you declare your own next to where it may be executed, so I kept a babel config where it was before, just referencing the root one.
    
    Docusaurus is stil using its own `docusaurus/core/lib/babel/preset`.
    
    Differential Revision: D52070490
    
    fbshipit-source-id: 0c211b2cbe19ade08867c9980fb922f628e0980e

--- javascript/babel.config.cjs
+++ javascript/babel.config.cjs
@@ -7,24 +7,6 @@
  * @format
  */
 
-module.exports = api => ({
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: [
-          'maintained node versions',
-          '> 0.5%, last 2 versions, Firefox ESR, not dead',
-        ],
-        // Do not transform to another module system
-        modules: false,
-      },
-    ],
-    [
-      '@babel/preset-typescript',
-      {
-        rewriteImportExtensions: api.env('dist'),
-      },
-    ],
-  ],
-});
+module.exports = {
+  presets: ['../babel.config.cjs'],
+};
