83ec77d2c420c347a9c04609e3fae42efb9c4429
Nick Gerleman <ngerlem@meta.com>
2023-12-14 04:36:47 -0800

    Enable lints for React Components (#1515)
    Summary:
    
    The out-of-the-box docusaurus template doesn't enable linting for React components. This enables those, fixes the errors, and does dome cleanup around the area (e.g. autofocus is a lot more sane).
    
    Differential Revision: D52156109

--- website-next/src/pages/index.tsx
+++ website-next/src/pages/index.tsx
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import Layout from '@theme/Layout';
