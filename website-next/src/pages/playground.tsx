17efa9c3d71009e3a84cd92f28bbf0d600f844b9
Nick Gerleman <ngerlem@meta.com>
2023-12-14 03:46:34 -0800

    Don't include code in SSR generation of Playground endpoint
    Summary: We allow arbitrary code in the query param, that static site generation doesn't know about. Current experience in production build is confusing, since you can see a flash of one set of code (playground default), quickly change to another. It is less confusing to have it go from blank to code showing.
    
    Differential Revision: D52162928

--- website-next/src/pages/playground.tsx
+++ website-next/src/pages/playground.tsx
@@ -12,18 +12,36 @@ import lzString from 'lz-string';
 import Playground from '../components/Playground';
 
 import styles from './playground.module.css';
+import useIsBrowser from '@docusaurus/useIsBrowser';
 
 export default function PlaygroundPage(): JSX.Element {
-  const params = new URLSearchParams(useLocation().search);
-  const codeParam = params.get('code');
-  const code = codeParam
-    ? lzString.decompressFromEncodedURIComponent(codeParam)
-    : undefined;
+  const code = useCodeFromQueryParam();
 
   return (
     // @ts-ignore missing prop for `wrapperClassName`
     <Layout wrapperClassName={styles.bg} title="Playground">
-      <Playground height="max(80vh, 600px)" code={code} autoFocus={true} />
+      <Playground
+        height="max(80vh, 600px)"
+        code={code}
+        autoFocus={true}
+        key={String(useIsBrowser())}
+      />
     </Layout>
   );
+}
+
+function useCodeFromQueryParam(): string | undefined {
+  const location = useLocation();
+
+  // We don't know the query param ahead of time when doing SSR, so just render
+  // blank to avoid the appearance of code changing.
+  if (!useIsBrowser()) {
+    return '';
+  }
+
+  const params = new URLSearchParams(location.search);
+  const codeParam = params.get('code');
+  return codeParam
+    ? lzString.decompressFromEncodedURIComponent(codeParam)
+    : undefined;
 }
