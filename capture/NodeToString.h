0d8ea32d91b792a1280f508681f54610243b973c
Joe Vilches <joevilches@fb.com>
2024-02-09 14:03:01 -0800

    Support for (de)serializing config values
    Differential Revision: https://www.internalfb.com/diff/D53203385?entry_point=27
    
    fbshipit-source-id: bf191ff3da97d2277045677ccafbf5b7c1509164

--- capture/NodeToString.h
+++ capture/NodeToString.h
@@ -13,6 +13,14 @@
 
 namespace facebook::yoga {
 
-void nodeToString(std::string& str, YGNodeRef node, YGPrintOptions options);
+enum class PrintOptions : uint8_t {
+  Layout = 1 << 0,
+  Children = 1 << 1,
+  Style = 1 << 2,
+  Config = 1 << 3,
+};
+YG_DEFINE_ENUM_FLAG_OPERATORS(PrintOptions);
+
+void nodeToString(std::string& str, YGNodeRef node, PrintOptions options);
 
 } // namespace facebook::yoga
