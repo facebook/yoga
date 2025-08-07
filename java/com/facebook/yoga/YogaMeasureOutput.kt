8ae1795a843383d8a8018bb1bce9fd5e5dd00070
Mateo Guzmán <info@mateoguzman.net>
2025-08-07 23:39:05 +0200

    Migrate YogaMeasureOutput to Kotlin

---
+++ java/com/facebook/yoga/YogaMeasureOutput.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.yoga
+
+/** Helpers for building measure output value. */
+public object YogaMeasureOutput {
+  public fun make(width: Float, height: Float): Long {
+    val wBits = java.lang.Float.floatToRawIntBits(width)
+    val hBits = java.lang.Float.floatToRawIntBits(height)
+    return (wBits.toLong()) shl 32 or (hBits.toLong())
+  }
+
+  public fun make(width: Int, height: Int): Long = make(width.toFloat(), height.toFloat())
+
+  public fun getWidth(measureOutput: Long): Float =
+      java.lang.Float.intBitsToFloat((0xFFFFFFFFL and (measureOutput shr 32)).toInt())
+
+  public fun getHeight(measureOutput: Long): Float =
+      java.lang.Float.intBitsToFloat((0xFFFFFFFFL and measureOutput).toInt())
+}
