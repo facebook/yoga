1ab0064349546ecb4cdf8b1f2d945f5a12196c75
Nick Gerleman <ngerlem@meta.com>
2026-02-24 23:58:21 -0800

    CSS Grid 7/9: Test generation infrastructure
    Summary:
    Update gentest scripts to support CSS Grid properties. Adds grid style
    extraction, grid property setup, display:grid handling, and grid-aware
    LTR/RTL fixture support to the C++, Java, and JavaScript test emitters.
    
    Differential Revision: D93946258

--- gentest/gentest-java.js
+++ gentest/gentest-java.js
@@ -166,6 +166,8 @@ JavaEmitter.prototype = Object.create(Emitter.prototype, {
   YGAlignSpaceAround: {value: 'YogaAlign.SPACE_AROUND'},
   YGAlignSpaceEvenly: {value: 'YogaAlign.SPACE_EVENLY'},
   YGAlignBaseline: {value: 'YogaAlign.BASELINE'},
+  YGAlignStart: {value: 'YogaAlign.START'},
+  YGAlignEnd: {value: 'YogaAlign.END'},
 
   YGDirectionInherit: {value: 'YogaDirection.INHERIT'},
   YGDirectionLTR: {value: 'YogaDirection.LTR'},
@@ -193,6 +195,10 @@ JavaEmitter.prototype = Object.create(Emitter.prototype, {
   YGJustifySpaceAround: {value: 'YogaJustify.SPACE_AROUND'},
   YGJustifySpaceBetween: {value: 'YogaJustify.SPACE_BETWEEN'},
   YGJustifySpaceEvenly: {value: 'YogaJustify.SPACE_EVENLY'},
+  YGJustifyStretch: {value: 'YogaJustify.STRETCH'},
+  YGJustifyStart: {value: 'YogaJustify.START'},
+  YGJustifyEnd: {value: 'YogaJustify.END'},
+  YGJustifyAuto: {value: 'YogaJustify.AUTO'},
 
   YGOverflowHidden: {value: 'YogaOverflow.HIDDEN'},
   YGOverflowVisible: {value: 'YogaOverflow.VISIBLE'},
@@ -219,6 +225,8 @@ JavaEmitter.prototype = Object.create(Emitter.prototype, {
   YGMaxContent: {value: 'MAX_CONTENT'},
   YGFitContent: {value: 'FIT_CONTENT'},
   YGStretch: {value: 'STRETCH'},
+
+  YGDisplayGrid: {value: 'YogaDisplay.GRID'},
 
   YGNodeCalculateLayout: {
     value: function (node, dir, _experiments) {
@@ -337,6 +345,18 @@ JavaEmitter.prototype = Object.create(Emitter.prototype, {
   YGNodeStyleSetJustifyContent: {
     value: function (nodeName, value) {
       this.push(nodeName + '.setJustifyContent(' + toValueJava(value) + ');');
+    },
+  },
+
+  YGNodeStyleSetJustifyItems: {
+    value: function (nodeName, value) {
+      this.push(nodeName + '.setJustifyItems(' + toValueJava(value) + ');');
+    },
+  },
+
+  YGNodeStyleSetJustifySelf: {
+    value: function (nodeName, value) {
+      this.push(nodeName + '.setJustifySelf(' + toValueJava(value) + ');');
     },
   },
 
@@ -472,6 +492,183 @@ JavaEmitter.prototype = Object.create(Emitter.prototype, {
       this.push(
         `${nodeName}.setMeasureFunction(new TestUtils.intrinsicMeasureFunction());`,
       );
+    },
+  },
+
+  YGNodeStyleSetGridTemplateRows: {
+    value: function (nodeName, tracks) {
+      if (!tracks || tracks.length === 0) {
+        return;
+      }
+
+      this.push(
+        `YogaGridTrackList ${nodeName}GridTemplateRows = new YogaGridTrackList();`,
+      );
+
+      for (const track of tracks) {
+        if (track.type === 'minmax') {
+          const minVal = this.formatGridTrackValueJava(track.min);
+          const maxVal = this.formatGridTrackValueJava(track.max);
+          this.push(
+            `${nodeName}GridTemplateRows.addTrack(YogaGridTrackValue.minMax(${minVal}, ${maxVal}));`,
+          );
+        } else {
+          const val = this.formatGridTrackValueJava(track);
+          this.push(`${nodeName}GridTemplateRows.addTrack(${val});`);
+        }
+      }
+
+      this.push(
+        `${nodeName}.setGridTemplateRows(${nodeName}GridTemplateRows);`,
+      );
+    },
+  },
+
+  YGNodeStyleSetGridTemplateColumns: {
+    value: function (nodeName, tracks) {
+      if (!tracks || tracks.length === 0) {
+        return;
+      }
+
+      this.push(
+        `YogaGridTrackList ${nodeName}GridTemplateColumns = new YogaGridTrackList();`,
+      );
+
+      for (const track of tracks) {
+        if (track.type === 'minmax') {
+          const minVal = this.formatGridTrackValueJava(track.min);
+          const maxVal = this.formatGridTrackValueJava(track.max);
+          this.push(
+            `${nodeName}GridTemplateColumns.addTrack(YogaGridTrackValue.minMax(${minVal}, ${maxVal}));`,
+          );
+        } else {
+          const val = this.formatGridTrackValueJava(track);
+          this.push(`${nodeName}GridTemplateColumns.addTrack(${val});`);
+        }
+      }
+
+      this.push(
+        `${nodeName}.setGridTemplateColumns(${nodeName}GridTemplateColumns);`,
+      );
+    },
+  },
+
+  YGNodeStyleSetGridColumnStart: {
+    value: function (nodeName, value) {
+      this.push(`${nodeName}.setGridColumnStart(${value});`);
+    },
+  },
+
+  YGNodeStyleSetGridColumnStartSpan: {
+    value: function (nodeName, value) {
+      this.push(`${nodeName}.setGridColumnStartSpan(${value});`);
+    },
+  },
+
+  YGNodeStyleSetGridColumnEnd: {
+    value: function (nodeName, value) {
+      this.push(`${nodeName}.setGridColumnEnd(${value});`);
+    },
+  },
+
+  YGNodeStyleSetGridColumnEndSpan: {
+    value: function (nodeName, value) {
+      this.push(`${nodeName}.setGridColumnEndSpan(${value});`);
+    },
+  },
+
+  YGNodeStyleSetGridRowStart: {
+    value: function (nodeName, value) {
+      this.push(`${nodeName}.setGridRowStart(${value});`);
+    },
+  },
+
+  YGNodeStyleSetGridRowStartSpan: {
+    value: function (nodeName, value) {
+      this.push(`${nodeName}.setGridRowStartSpan(${value});`);
+    },
+  },
+
+  YGNodeStyleSetGridRowEnd: {
+    value: function (nodeName, value) {
+      this.push(`${nodeName}.setGridRowEnd(${value});`);
+    },
+  },
+
+  YGNodeStyleSetGridRowEndSpan: {
+    value: function (nodeName, value) {
+      this.push(`${nodeName}.setGridRowEndSpan(${value});`);
+    },
+  },
+
+  YGNodeStyleSetGridAutoColumns: {
+    value: function (nodeName, tracks) {
+      if (!tracks || tracks.length === 0) {
+        return;
+      }
+
+      this.push(
+        `YogaGridTrackList ${nodeName}GridAutoColumns = new YogaGridTrackList();`,
+      );
+
+      for (const track of tracks) {
+        if (track.type === 'minmax') {
+          const minVal = this.formatGridTrackValueJava(track.min);
+          const maxVal = this.formatGridTrackValueJava(track.max);
+          this.push(
+            `${nodeName}GridAutoColumns.addTrack(YogaGridTrackValue.minMax(${minVal}, ${maxVal}));`,
+          );
+        } else {
+          const val = this.formatGridTrackValueJava(track);
+          this.push(`${nodeName}GridAutoColumns.addTrack(${val});`);
+        }
+      }
+
+      this.push(`${nodeName}.setGridAutoColumns(${nodeName}GridAutoColumns);`);
+    },
+  },
+
+  YGNodeStyleSetGridAutoRows: {
+    value: function (nodeName, tracks) {
+      if (!tracks || tracks.length === 0) {
+        return;
+      }
+
+      this.push(
+        `YogaGridTrackList ${nodeName}GridAutoRows = new YogaGridTrackList();`,
+      );
+
+      for (const track of tracks) {
+        if (track.type === 'minmax') {
+          const minVal = this.formatGridTrackValueJava(track.min);
+          const maxVal = this.formatGridTrackValueJava(track.max);
+          this.push(
+            `${nodeName}GridAutoRows.addTrack(YogaGridTrackValue.minMax(${minVal}, ${maxVal}));`,
+          );
+        } else {
+          const val = this.formatGridTrackValueJava(track);
+          this.push(`${nodeName}GridAutoRows.addTrack(${val});`);
+        }
+      }
+
+      this.push(`${nodeName}.setGridAutoRows(${nodeName}GridAutoRows);`);
+    },
+  },
+
+  formatGridTrackValueJava: {
+    value: function (track) {
+      switch (track.type) {
+        case 'auto':
+          return 'YogaGridTrackValue.auto()';
+        case 'points':
+          return `YogaGridTrackValue.points(${toValueJava(track.value)}f)`;
+        case 'percent':
+          return `YogaGridTrackValue.percent(${toValueJava(track.value)}f)`;
+        case 'fr':
+          return `YogaGridTrackValue.fr(${toValueJava(track.value)}f)`;
+        default:
+          return 'YogaGridTrackValue.auto()';
+      }
     },
   },
 });
