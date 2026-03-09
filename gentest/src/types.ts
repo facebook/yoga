1f2d2c83b4005d8d7c17feaabc6b78ad6b7f6a73
Nick Gerleman <ngerlem@meta.com>
2026-03-09 12:11:35 -0700

    Make gentest faster and more reliable (#1889)
    Summary:
    Pull Request resolved: https://github.com/facebook/yoga/pull/1889
    
    This change used Claude to make some major quality of life changes to `yarn gentest`. The bulk of the logic is now run in the driver, instead of the browser, and the scripts are typechecked. We rely on attributes, instead of declared style in DOM, so that we don't miss inputs the browser massages. We no longer preprocess the fixtures, to remap invalid CSS properties to valid ones. Generation now happens in parallel. We avoid outputting temp files to source root.
    
    From there, I looked over major parts, and had it clean up a lot of things to taste, but I let it try to do a full rewrite for simplicity, using the original as reference.
    
    It is about neutral in terms of line count
    
    Reviewed By: sammy-SC
    
    Differential Revision: D94005983

---
+++ gentest/src/types.ts
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+export type ValueWithUnit =
+  | {type: 'points'; value: number}
+  | {type: 'percent'; value: number}
+  | {type: 'auto'}
+  | {type: 'max-content'}
+  | {type: 'fit-content'}
+  | {type: 'stretch'}
+  | {type: 'undefined'};
+
+export type ParsedStyles = Map<string, string>;
+
+export interface LayoutNode {
+  id: string;
+  left: number;
+  top: number;
+  width: number;
+  height: number;
+  styleAttr: string;
+  experiments: string[];
+  disabled: boolean;
+  innerText: string;
+  children: LayoutNode[];
+}
+
+export interface TestCase {
+  name: string;
+  ltrLayout: LayoutNode;
+  rtlLayout: LayoutNode;
+  styleAttr: string;
+  experiments: string[];
+  disabled: boolean;
+}
