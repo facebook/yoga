2397137f0a9459902bdfdf0d444b584d007e7195
Nicola Corti <ncor@meta.com>
2026-05-12 05:52:36 -0700

    Migrate YogaGutter enum to Kotlin
    Summary:
    X-link: https://github.com/facebook/react-native/pull/56791
    
    Migrate YogaGutter.java to YogaGutter.kt by adding Gutter to KOTLIN_ENUM_NAMES in enums.py and regenerating.
    
    Differential Revision: D104666345

--- enums.py
+++ enums.py
@@ -106,6 +106,7 @@ KOTLIN_ENUM_NAMES = {
     "ExperimentalFeature",
     "FlexDirection",
     "GridTrackType",
+    "Gutter",
 }
 
 ENUMS_KOTLIN = {name: ENUMS[name] for name in KOTLIN_ENUM_NAMES}
